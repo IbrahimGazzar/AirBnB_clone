d31cf09124c83ef81f4aae3607dbfb04916d767f
Ibrahim Al-Gazzar <94261285+IbrahimGazzar@users.noreply.github.com>
2023-11-09 21:10:24 +0200

    Create BaseModel file

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains the BaseModel
+class for the AirBnB clone project,
+from which all other models will inherit
+their base and common features and
+attributes from
+"""
+import datetime
+import uuid
+
+class BaseModel:
+  """
+  The base model from 
+  """
